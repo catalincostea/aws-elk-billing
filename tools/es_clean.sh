bec2e737b401decd615e79f495cdf63cf2b46f8a
catalincostea <catalin.costea@gmail.com>
2019-04-01 12:26:54 +0300

    Update es_clean.sh

--- tools/es_clean.sh
+++ tools/es_clean.sh
@@ -1,1 +1,2 @@
-curl -X DELETE http://localhost:9200/`curl http://localhost:9200/_cat/indices | grep billing | awk '{print $3}' | sort -r | tail -n+2` > /var/log/myjob.log 2>&1
+curl -X DELETE http://localhost:9200/`curl http://localhost:9200/_cat/indices | grep billing | awk '{print $3}' | sort -r | tail -n+2`
+# > /var/log/myjob.log 2>&1
